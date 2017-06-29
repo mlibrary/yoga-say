e81a6f6e91e0b4a6f0f4dabdf509bebe77f0640a
Noah Botimer <botimer@umich.edu>
2017-06-29 15:08:05 -0400

    Make things "tidier"

--- lib/say.rb
+++ lib/say.rb
@@ -1,62 +1,71 @@
 class Say
   attr_reader :num
-
   def initialize(num)
     @num = num
   end
-
   def in_english
     if num < 0 || num > 999_999_999_999
       raise ArgumentError.new("Numbers must be between 0 and 999,999,999,999")
     end
     phrase(num)
   end
-
-  def phrase(remainder)
+  def phrase(number)
     words = []
     loop do
-      if remainder >= 1_000_000_000
-        words << phrase(remainder / 1_000_000_000) + " billion"
-        remainder = remainder % 1_000_000_000
-      elsif remainder >= 1_000_000
-        words << phrase(remainder / 1_000_000) + " million"
-        remainder = remainder % 1_000_000
-      elsif remainder >= 1000
-        words << phrase(remainder / 1000) + " thousand"
-        remainder = remainder % 1000
-      elsif remainder >= 100
-        howmany = remainder / 100
-        words << phrase(remainder / 100) + " hundred"
-        remainder = remainder % 100
-      elsif remainder >= 20
-        if remainder % 10 == 0
-          words << tens(remainder)
+      if number >= 1_000_000_000
+        if number > num
+          words << phrase(number / num) + " million"
+          number = num
         else
-          words << tens(remainder) + "-" + upto_nineteen(remainder % 10)
+          words << phrase(number / 1_000_000_000) + " billion"
+          number = number % 1_000_000_000
         end
-        remainder = 0
+      elsif number >= 1_000_000
+        if number > num
+          words << phrase(number / num) + " thousand"
+          number = num
+        else
+          words << phrase(number / 1_000_000) + " million"
+          number = number % 1_000_000
+        end
+      elsif number >= 1000
+        if number > num
+          words << phrase(number / num) + " hundred"
+          number = num
+        else
+          words << phrase(number / 1000) + " thousand"
+          number = number % 1000
+        end
+      elsif number >= 100
+        if number > num
+          words << phrase(number / num)
+          number = num
+        else
+          words << phrase(number / 100) + " hundred"
+          number = number % 100
+        end
+      elsif number >= 20
+        if number % 10 == 0
+          words << tens(number)
+        else
+          words << tens(number) + "-" + upto_nineteen(number % 10)
+        end
+        number = 0
       else
-        words << upto_nineteen(remainder)
-        remainder = 0
+        words << upto_nineteen(number)
+        number = 0
       end
-      break if remainder == 0
+      break if number == 0
     end
     words.join(' ')
   end
-
   def upto_nineteen(i)
     ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
      'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
      'eighteen', 'nineteen'][i]
   end
-
   def tens(i)
     decade = i / 10
     prefix = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'][decade]
   end
-
-end
-
-module BookKeeping
-  VERSION = 1
 end
