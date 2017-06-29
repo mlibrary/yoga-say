7ec94b20415c343c100b74c4f83037565c624288
gkostin1966 <gkostin@umich.edu>
2017-06-29 16:02:21 -0400

    what

--- spec/say_spec.rb
+++ spec/say_spec.rb
@@ -1,9 +1,14 @@
 require 'say'
 
 RSpec.describe Say do
+  let(:number) { double("number") }
+  let(:say) { Say.new(number) }
   describe "#in_english" do
-    subject { Say.new(0) }
-    it { expect(subject.english).to eq('zero') }
+    context 'zero' do
+      let(:number) { 0 }
+      subject { say.in_english }
+        it { expect(subject).to eq('zero') }
+    end
   end
 end
 
