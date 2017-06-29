32d17215ea9abe10d2f700a3c8e33cf9d33585e1
gkostin1966 <gkostin@umich.edu>
2017-06-29 16:23:28 -0400

    min max

--- spec/say_spec.rb
+++ spec/say_spec.rb
@@ -1,20 +1,52 @@
 require 'say'
 
 RSpec.describe Say do
+  MINIMUM = 0
+  MAXIMUM = 999_999_999_999
   let(:number) { double("number") }
   let(:say) { Say.new(number) }
 
   describe "#in_english" do
+    context 'one under maximum' do
+      let(:number) { -1 }
+      subject { say.in_english }
+      it { expect(subject).to eq("minus one") }
+    end
+
+    context 'one over maximum' do
+      let(:number) { -1 }
+      subject { say.in_english }
+      it { expect(subject).to eq("minus one") }
+    end
+
+    context 'min' do
+      let(:number) { 0 }
+      subject { say.in_english }
+      it { expect(subject).to eq("zero") }
+    end
+
+    context 'max' do
+      let(:number) { 999_999_999_999 }
+      subject { say.in_english }
+      it { expect(subject).to eq("nine hundred ninety-nine billion nine hundred ninety-nine million nine hundred ninety-nine thousand nine hundred ninety-nine") }
+    end
+
     context 'zero' do
       let(:number) { 0 }
       subject { say.in_english }
       it { expect(subject).to eq('zero') }
     end
-    
+
     context 'one' do
       let(:number) { 1 }
       subject { say.in_english }
       it { expect(subject).to eq('one') }
+    end
+
+    context 'two' do
+      let(:number) { 2 }
+      subject { say.in_english }
+      it { expect(subject).to eq('two') }
     end
   end
 end
